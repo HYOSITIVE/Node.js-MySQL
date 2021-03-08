94326f554ea941cda54fa5b3beade2080f54b7ca
Hyosik Philip Joo <lux1937@gmail.com>
2021-03-08 22:55:00 +0900

    Video 14.2 : Simplifying - topic 2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Last Modification : 2021.03.08
 // by HYOSITIVE
-// based on Opentutorials - Node.js & MySQL - 14
+// based on Opentutorials - Node.js & MySQL - 14.2
 
 var http = require('http');
 var url = require('url');
@@ -14,88 +14,22 @@ var app = http.createServer(function(request,response){
 	var queryData = url.parse(_url, true).query;
 	var pathname = url.parse(_url, true).pathname;
 	
-	// root, 즉 path가 없는 경로로 접속했을 때 - 정상 접속
-	if (pathname === '/') {
+	if (pathname === '/') { // root, 즉 path가 없는 경로로 접속했을 때 - 정상 접속
 		if(queryData.id === undefined) { // 메인 페이지
 			topic.home(request, response);
 		}
 		
 		else { // 컨텐츠를 선택한 경우
-			db.query(`SELECT * FROM topic`, function(error, topics) {
-				if (error) {
-					throw error;
-				}
-				db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id = author.id WHERE topic.id=?`,[queryData.id], function(error2, topic) {
-					// SQL문의 ?에 두 번째 인자의 값이 들어감. 해킹으로 인한 데이터 유출 방지
-					if (error2) {
-						throw error2;
-					}
-					// topic이 배열로 return되기 때문에, 첫 번째 원소를 명시해주어야 함
-					var title = topic[0].title;
-					var description = topic[0].description;
-					var list = template.list(topics);
-					var html = template.HTML(title, list,
-						`<h2>${title}</h2>${description}
-						<p>by ${topic[0].name}</p>`,
-						` <a href="/create">create</a>
-						  <a href="/update?id=${queryData.id}">update</a>
-						  <form action="delete_process" method="post">
-							  <input type="hidden" name="id" value="${queryData.id}">
-							  <input type="submit" value="delete">			
-						  </form>`
-					);
-				response.writeHead(200);
-				response.end(html);
-				})
-			});
+			topic.page(request, response);
 		}	
 	}
-	// 새로운 컨텐츠 생성
-	else if(pathname === '/create') { 
-		db.query(`SELECT * FROM topic`, function(error, topics) {
-			db.query(`SELECT * FROM author`, function(error2, authors) {
-				var title = 'Create';
-				var list = template.list(topics);
-				var html = template.HTML(title, list,
-					`
-					<form action="/create_process" method="post">
-						<p><input type ="text" name="title" placeholder="title"></p>
-						<p><textarea name="description" placeholder="description"></textarea></p>
-						<p>
-							${template.authorSelect(authors)}
-						</p>
-						<p><input type="submit"></p>
-					</form>`,
-					`<a href="/create">create</a>`
-					);
-				response.writeHead(200);
-				response.end(html);
-			});
-		});
+	
+	else if(pathname === '/create') {  // 새로운 컨텐츠 생성
+		topic.create(request, response);
 	}
 
-	// 컨텐츠 생성 작업
-	else if(pathname === '/create_process') { 
-		var body = '';
-		// 데이터의 조각을 서버쪽에서 수신할 때마다, 서버는 callback 함수를 호출, data parameter를 통해 수신한 정보 제공
-		request.on('data', function(data) {
-			body = body + data; // callback이 실행될 때마다 데이터 추가
-		});
-		// 더 이상 들어올 정보가 없을 때, end에 해당되는 callback 함수 호출, 정보 수신이 끝났다는 뜻
-		request.on('end', function() {
-			var post = qs.parse(body);
-			db.query(`INSERT INTO topic (title, description, created, author_id)
-				VALUES(?, ?, NOW(), ?)`,
-				[post.title, post.description, post.author],
-				function(error, result) {
-					if (error) {
-						throw error;
-					}
-					response.writeHead(302, {Location: `/?id=${result.insertId}`}); // 삽입한 행의 id값
-					response.end();
-				}
-			)
-		});
+	else if(pathname === '/create_process') { // 컨텐츠 생성 작업
+		topic.create_process(request, response);
 	}
 
 	// 업데이트
