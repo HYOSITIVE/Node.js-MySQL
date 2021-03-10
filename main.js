38905de292fad8cfcabb284a781433928270d56a
Hyosik Philip Joo <lux1937@gmail.com>
2021-03-10 22:50:00 +0900

    Video 18.1 : Implement author update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Last Modification : 2021.03.08
 // by HYOSITIVE
-// based on Opentutorials - Node.js & MySQL - 17
+// based on Opentutorials - Node.js & MySQL - 18.1
 
 var http = require('http');
 var url = require('url');
@@ -50,6 +50,10 @@ var app = http.createServer(function(request,response){
 
 	else if(pathname === '/author/create_process') {
 		author.create_process(request, response);
+	}
+
+	else if(pathname === '/author/update') {
+		author.update(request, response);
 	}
 
 	else { // 그 외의 경로로 접속했을 때 - 에러
